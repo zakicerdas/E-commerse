4a10360c80f6d3c923315da8febecd9bd6bc4b67
zakicerdas <zakitoriq1@gmail.com>
2025-12-29 09:07:05 +0700

    fix eror handler

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
