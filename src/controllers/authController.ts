e681d556fb4c883274ecbe943655ca8b4a619935
zakicerdas <zakitoriq1@gmail.com>
2025-12-29 09:21:46 +0700

    fix error registerr

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,14 +3,27 @@ import * as AuthService from '../services/auth.service';
 import { asyncHandler } from '../utils/async.handler';
 import { successResponse } from '../utils/response';
 
-export const register = asyncHandler(async (req: Request, res: Response) => {
-  const user = await AuthService.register(req.body);
-  const { password, ...userWithoutPassword } = user;
-  
-  return successResponse(res, 'Register berhasil', userWithoutPassword, null, 201);
-});
+export const register = asyncHandler(
+  async (req: Request, res: Response) => {
+    const user = await AuthService.register(req.body);
+    return successResponse(
+      res,
+      'Register berhasil',
+      user,
+      null,
+      201
+    );
+  }
+);
 
-export const login = asyncHandler(async (req: Request, res: Response) => {
-  const result = await AuthService.login(req.body);
-  return successResponse(res, 'Login berhasil', result);
-});
+export const login = asyncHandler(
+  async (req: Request, res: Response) => {
+    const result = await AuthService.login(req.body);
+
+    return successResponse(
+      res,
+      'Login berhasil',
+      result
+    );
+  }
+);
